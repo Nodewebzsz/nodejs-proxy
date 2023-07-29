81590b4c7b50f178ae607f75696e0c94df1ed780
Nodewebzsz <zszxingchenid@gmail.com>
2023-07-29 18:14:52 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ae425651-f870-48bc-b428-8d9a975ea194';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
